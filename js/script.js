4a171db5046f0e5740cd6f4ce4d8344a1b6f01c8
Krutagna <kpatel.kp7016@gmail.com>
2023-01-04 18:42:22 +0530

    .

--- js/script.js
+++ js/script.js
@@ -7,25 +7,45 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
-const openModal = function () {
+const openModal = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btnOpenModal) => {
+  btnOpenModal.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = `
+          We use cookies for improved functionality and analytics.
+          <button class='btn btn--close-cookie'>Got it!</button>
+`;
+header.append(message);
+
+// Delete element
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+})
+
+
+
